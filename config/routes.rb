ca7fd4ad08c354ec5d1cbfabf654116b25609ed0
tysonvanburen <tysonvanburen444@gmail.com>
2014-06-06 16:12:49 -0600

    asdf

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,8 @@ Rails.application.routes.draw do
 
   resources :tasks
 
-<<<<<<< HEAD
-  root 'tasks#index'
-=======
   root 'static_pages#home'
->>>>>>> develop
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
